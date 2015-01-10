3ccb140545db8966fda8780a554b57dc6669d83e
dermdunc <dermduncan@gmail.com>
2015-01-10 12:29:26 -0500

    Added monitoring script
    Script to monitor file changes. Takes 2 inputs - the command to be run
    when a file changes and the file to monitor

---
+++ laptop/monitor.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+f=$1
+shift
+tmpf="`mktemp /tmp/onchange.XXXXX`"
+cp "$f" "$tmpf"
+trap "rm $tmpf; exit 1" 2
+while : ; do
+    if [ "$f" -nt "$tmpf" ];
+    then
+        touch -r "$f" "$tmpf"
+        $@
+    fi
+    sleep 0.5
+done
