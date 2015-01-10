11f6e6a58607abf49da45ff78251a104afb2df25
Art-Wolf <john.c.doyle@gmail.com>
2015-01-10 11:08:09 -0500

    Updated

--- wear/src/main/java/ie/johndoyle/flow/MainActivity.java
+++ wear/src/main/java/ie/johndoyle/flow/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.os.Vibrator;
 import android.support.wearable.view.WatchViewStub;
 import android.util.Log;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 public class MainActivity extends Activity implements SensorEventListener {
@@ -19,9 +20,7 @@ public class MainActivity extends Activity implements SensorEventListener {
     private Sensor accelerometer;
     private Sensor gyro;
 
-    private float deltaXMax = 0;
-    private float deltaYMax = 0;
-    private float deltaZMax = 0;
+    private float trackX = 0;
 
     private float deltaX = 0;
     private float deltaY = 0;
@@ -33,7 +32,7 @@ public class MainActivity extends Activity implements SensorEventListener {
 
     private float vibrateThreshold = 0;
 
-    private TextView currentX, currentY, currentZ, currentGX, currentGY, currentGZ;
+    private TextView currentX, currentY, currentZ, currentGX, currentGY, currentGZ, nextSlide;
 
     public Vibrator v;
 
@@ -41,6 +40,7 @@ public class MainActivity extends Activity implements SensorEventListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.round_activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         initializeViews();
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
@@ -64,6 +64,7 @@ public class MainActivity extends Activity implements SensorEventListener {
         currentGX = (TextView) findViewById(R.id.currentGX);
         currentGY = (TextView) findViewById(R.id.currentGY);
         currentGZ = (TextView) findViewById(R.id.currentGZ);
+        nextSlide = (TextView) findViewById(R.id.nextSlide);
     }
 
     protected void onResume() {
@@ -95,13 +96,19 @@ public class MainActivity extends Activity implements SensorEventListener {
         if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
 
             // get the change of the x,y,z values of the accelerometer
-            deltaX = Math.abs(lastX - event.values[0]);
+            deltaX = lastX - event.values[0];//Math.abs(lastX - event.values[0]);
             deltaY = Math.abs(lastY - event.values[1]);
-            deltaZ = Math.abs(lastZ - event.values[2]);
+            deltaZ = lastZ - event.values[2];
 
             lastX = event.values[0];
             lastY = event.values[1];
             lastZ = event.values[2];
+
+            if (deltaZ >= 1) {
+                trackX += deltaZ;
+            }
+            else if (deltaZ >= 0) {}
+            else { trackX = 0; }
 
             // if the change is below 2, it is just plain noise
             if (deltaX < 2)
@@ -150,12 +157,13 @@ public class MainActivity extends Activity implements SensorEventListener {
         currentGX.setText(Float.toString(deltaGX));
         currentGY.setText(Float.toString(deltaGY));
         currentGZ.setText(Float.toString(deltaGZ));
-        Log.i("Flow", "Flow.displayCurrentValues() —  currentX:" + currentX);
-        Log.i("Flow", "Flow.displayCurrentValues() —  currentY:" + currentY);
-        Log.i("Flow", "Flow.displayCurrentValues() —  currentZ:" + currentZ);
-        Log.i("Flow", "Flow.displayCurrentValues() —  currentGX:" + currentGX);
-        Log.i("Flow", "Flow.displayCurrentValues() —  currentGY:" + currentGY);
-        Log.i("Flow", "Flow.displayCurrentValues() —  currentGX:" + currentGZ);
+
+        if (trackX > 20) {
+            nextSlide.setText("True");
+            Log.i("Flow", "Next Slide");
+        } else if (trackX == 0) {
+            nextSlide.setText("False");
+        }
     }
 }
 
